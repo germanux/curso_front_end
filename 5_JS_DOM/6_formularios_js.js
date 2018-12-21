82088ce249d44c3ccde9054869c2cc3195c15bff
German Caballero <germanux@gmail.com>
2018-12-21 10:02:37 +0100

    idem

--- 5_JS_DOM/6_formularios_js.js
+++ 5_JS_DOM/6_formularios_js.js
@@ -23,6 +23,21 @@ function validarInputById(id) {
     return true;
   }
 }
+
+function validarParInput(id_1, id_2) {
+  var input_1 = document.getElementById(id_1);
+  var input_2 = document.getElementById(id_2);
+  if (input_1.value != input_2.value) {
+    input_1.style.borderColor = "orange";
+    input_2.style.borderColor = "orange";
+    return false;
+  } else {
+    input_1.style.borderColor = "green";
+    input_2.style.borderColor = "green";
+    return true;
+  }
+}
+
 function validarFormulario() {
   
   if (validarInputById("email") 
@@ -38,18 +53,4 @@ function validarFormulario() {
      document.getElementById("mensaje").value = "Registrado";
      document.getElementsByTagName("table")[0].style.backgroundColor = "green";
    }
-}
-
-function validarParInput(id_1, id_2) {
-  var input_1 = document.getElementById(id_1);
-  var input_2 = document.getElementById(id_2);
-  if (input_1.value != input_2.value) {
-    input_1.style.borderColor = "orange";
-    input_2.style.borderColor = "orange";
-    return false;
-  } else {
-    input_1.style.borderColor = "green";
-    input_2.style.borderColor = "green";
-    return true;
-  }
 }
