b4f5e8219de575dc4e7bf4c9418d03c6fd8ae5b1
German Caballero <germanux@gmail.com>
2019-02-04 11:18:10 +0100

    Un poco typescript y bucles rendimiento

---
+++ 9_TypeScript/01_variables_y_fun.js
@@ -0,0 +1,9 @@
+var unNumero;
+unNumero = 12;
+// unNumero = "un texto";
+console.log("Un numero " + unNumero);
+var unTexto = "Un texto";
+console.log(untexto);
+alert(unTexto);
+
+variableSinDeclarar = "LLLLLLL";
---
+++ 9_TypeScript/01_variables_y_fun.ts
@@ -0,0 +1,16 @@
+
+let unNumero : number;
+
+unNumero = 12;
+//unNumero = "un texto";
+
+console.log("Un numero " + unnumero);
+
+var unTexto = "Un texto";
+
+console.log(unTexto);
+
+alert(unTexto);
+
+// variableSinDeclarar = "LLLLLLL";
+
